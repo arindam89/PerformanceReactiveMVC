b7f53882cb943738898da99e9af096936e7cf7d4
Arindam Paul <arindampaul1989@gmail.com>
2020-04-17 22:04:45 +0530

    Initial commit from Spring Start

---
+++ src/test/java/com/geekpaul/reactivemvc/ReactiveMvcSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekpaul.reactivemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveMvcSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
